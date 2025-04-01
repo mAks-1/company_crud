22f3277330f6aff5147245e9377f5f84de82ebbd
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-01 20:38:16 +0300

    included api router

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
+from app.core.config import settings
+from app.api import router as api_router
 
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 
 @app.get("/")
@@ -8,6 +15,10 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )

61d7bec1197fa4b8de5295fd279e1ff9048e168f
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 13:04:50 +0300

    added api for deleting and getting user by id

--- app/api/users.py
+++ app/api/users.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.models import User, db_helper
-from app.core.schemas.schemas import ReadUser, CreateUser
+from app.core.schemas.schemas import ReadUser, CreateUser, DeleteUser
 from app.crud import users as crud_users
 
 router = APIRouter(
@@ -30,3 +30,28 @@ async def create_user(
         session=session,
         user_create=user_create,
     )
+
+
+@router.get("/{user_id}", response_model=ReadUser)
+async def get_user_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user_id_to_get: int,
+):
+    user = await crud_users.get_user_by_id(
+        session=session,
+        user_id_to_get=user_id_to_get,
+    )
+
+    return user if user else None
+
+
+@router.delete("/{user_id}", response_model=DeleteUser)
+async def delete_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user_id_to_delete: int,
+):
+    user = await crud_users.delete_user_by_id(
+        session=session,
+        user_id_to_delete=user_id_to_delete,
+    )
+    return user if user else None

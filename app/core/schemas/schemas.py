0ada3480ba5f5584a1f197e90ff9b99cf6d247a8
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 13:19:57 +0300

    fixed schemas

--- app/core/schemas/schemas.py
+++ app/core/schemas/schemas.py
@@ -43,9 +43,9 @@ class CreateUser(UserBase):
 
 
 class UpdateUser(BaseModel):
-    username: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     email: Optional[str] = None
-    password: Optional[str] = None
     company_id: Optional[int] = None
 
 

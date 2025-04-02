14faecf5cf82f6fc6015db2a1ff78b8bb679c44e
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 10:37:20 +0300

    included company_router(should have been much earlier)

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 
 from app.core.config import settings
+from .companies import router as company_router
 
 router = APIRouter(
     prefix=settings.api.prefix,
+)
+
+router.include_router(
+    company_router,
 )

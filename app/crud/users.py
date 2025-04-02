1d264bc12f3168c51432a49c05425564c887544d
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 13:04:33 +0300

    added deleting and getting user by id

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,5 +1,6 @@
 from typing import Sequence
 
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,7 +11,7 @@ from app.core.schemas.schemas import CreateUser
 async def create_user(
     session: AsyncSession,
     user_create: CreateUser,
-):
+) -> User:
     user = User(**user_create.model_dump())
     session.add(user)
     await session.commit()
@@ -23,3 +24,34 @@ async def get_users(
     stmt = select(User).order_by(User.user_id)
     result = await session.execute(stmt)
     return result.scalars().all()
+
+
+async def get_user_by_id(
+    session: AsyncSession,
+    user_id_to_get: int,
+) -> User:
+    result = await session.execute(
+        select(User).filter(User.user_id == user_id_to_get),
+    )
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
+
+
+async def delete_user_by_id(
+    session: AsyncSession,
+    user_id_to_delete: int,
+):
+    result = await session.execute(
+        select(User).filter(User.user_id == user_id_to_delete),
+    )
+
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    await session.delete(user)
+    await session.commit()
+    return {"message": "User deleted successfully"}

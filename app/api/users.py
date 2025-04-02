cbd780f32a7be4dee4e6d2e02c4ee5c95f477363
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 12:53:53 +0300

    added api for creating and getting all users

--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,32 @@
-from fastapi import APIRouter
+from typing import Annotated
 
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.models import User, db_helper
+from app.core.schemas.schemas import ReadUser, CreateUser
+from app.crud import users as crud_users
 
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
+
+
+@router.get("/", response_model=list[ReadUser])
+async def get_users(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    users = await crud_users.get_users(session=session)
+    return users if users else []
+
+
+@router.post("/", response_model=ReadUser)
+async def create_user(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    user_create: CreateUser,
+):
+    return await crud_users.create_user(
+        session=session,
+        user_create=user_create,
+    )

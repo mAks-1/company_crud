ab15ec9a2ef5c9fb1d55003bc57c2029adaf54c7
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 10:09:01 +0300

    added api for getting and deleting companies

--- app/api/companies.py
+++ app/api/companies.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.models import Company, db_helper
-from app.core.schemas.schemas import ReadCompany, CreateCompany
+from app.core.schemas.schemas import ReadCompany, CreateCompany, DeleteCompany
 from app.crud import companies as crud_companies
 
 router = APIRouter(
@@ -30,3 +30,26 @@ async def create_company(
         session=session, company_create=company_create
     )
 
+
+@router.get("/{company_id}", response_model=ReadCompany)
+async def get_company_by_id(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    company_id_to_get: int,
+):
+    company = await crud_companies.get_company_by_id(
+        session=session, company_id_to_get=company_id_to_get
+    )
+    return company
+
+
+@router.delete("/{company_id}", response_model=DeleteCompany)
+async def delete_company(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    company_id_to_delete: int,
+):
+    company = await crud_companies.delete_company_by_id(
+        session=session,
+        company_id_to_delete=company_id_to_delete,
+    )
+
+    return company

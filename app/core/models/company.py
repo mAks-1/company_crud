54f25fd6561584d2257250d07b9409669c1e4509
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-02 12:29:21 +0300

    altered companys table

--- app/core/models/company.py
+++ app/core/models/company.py
@@ -1,10 +1,20 @@
+from typing import List, TYPE_CHECKING
+
 from .base import Base
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Company(Base):
+    company_id: Mapped[int] = mapped_column(primary_key=True)
+
     company_name: Mapped[str] = mapped_column(unique=True, nullable=False)
     company_address: Mapped[str] = mapped_column(unique=True, nullable=False)
     company_email: Mapped[str] = mapped_column(unique=True, nullable=False)
     company_phone: Mapped[str] = mapped_column(nullable=False)
     company_description: Mapped[str] = mapped_column(nullable=True)
+
+    # Відношення один-до-багатьох з User
+    users: Mapped[List["User"]] = relationship(back_populates="company")

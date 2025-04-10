5e40c2fc98f5c61baef2066fdb6691c470c7c340
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-10 11:33:43 +0300

    jwt api

---
+++ app/api/jwt.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from fastapi import APIRouter, Depends
+from app.core.schemas.schemas import UserSchema
+from app.auth.dependencies import (
+    get_current_active_auth_user,
+    get_current_token_payload,
+)
+from pydantic import BaseModel
+
+router = APIRouter(
+    prefix="/jwt",
+    tags=["jwt"],
+)
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.get("/users/me/")
+def auth_user_check_self_info(
+    payload: dict = Depends(get_current_token_payload),
+    user: UserSchema = Depends(get_current_active_auth_user),
+):
+    iat = payload.get("iat")
+    return {
+        "username": user.username,
+        "email": user.email,
+        "logged_in_at": datetime.utcfromtimestamp(iat).isoformat() if iat else None,
+    }

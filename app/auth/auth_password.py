e300b04c8bde370b3036d59c08588873e052eccf
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-10 11:21:06 +0300

    password auth(previously were in utils_jwt.py)

---
+++ app/auth/auth_password.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+
+def hash_password(
+    password: str,
+) -> str:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt).decode("utf-8")
+
+
+def validate_password(
+    password: str,
+    hashed_password: str,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password.encode(),
+    )

1f8bb57dc04b4f154ae791b40a3d2c73c6d8d6f9
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-11 13:24:45 +0300

    deleted unnecessary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import LoginPage from './pages/auth/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import UsersPage from './pages/users/UsersPage';
+// import CompaniesPage from './pages/CompaniesPage';
+import CompaniesPage from './pages/companies/CompaniesPage'
+import UserFormPage from './pages/users/UserFormPage';
+import CompanyFormPage from './pages/companies/CompanyFormPage';
+import Navbar from './components/Navbar';
+
+const AppRoutes = () => {
+  const { token } = useAuth();
+  
+  return (
+    <Routes>
+      <Route path="/login" element={!token ? <LoginPage /> : <Navigate to="/" />} />
+      
+      {token ? (
+        <>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/new" element={<UserFormPage />} />
+          <Route path="/users/:id/edit" element={<UserFormPage />} />
+          <Route path="/companies" element={<CompaniesPage />} />
+          <Route path="/companies/new" element={<CompanyFormPage />} />
+          <Route path="/companies/:id" element={<CompanyFormPage />} />
+        </>
+      ) : (
+        <Route path="*" element={<Navigate to="/login" />} />
+      )}
+    </Routes>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <AppRoutes />
+      </Router>
+    </AuthProvider>
   );
 }
 

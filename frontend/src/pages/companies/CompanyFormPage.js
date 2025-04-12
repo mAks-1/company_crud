9f3afed18f63587bf3aaca5f15854b2742008b67
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-12 18:56:08 +0300

    added Cancel button

--- frontend/src/pages/companies/CompanyFormPage.js
+++ frontend/src/pages/companies/CompanyFormPage.js
@@ -174,6 +174,9 @@ const CompanyFormPage = () => {
           <button type="submit" className="px-4 py-2 mt-4 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
             {isEditMode ? 'Update Company' : 'Create Company'}
           </button>
+          <button type="button" onClick={() => navigate('/users')}>
+            Cancel
+          </button>
         </form>
       </div>
     </div>

85ad28d714e9385263a3ab2e2e8691c2fe1d18ea
Maksym Chesherov <chesherow22d@gmail.com>
2025-04-21 12:32:50 +0300

    included ChatPage component

--- frontend/src/pages/users/UsersPage.js
+++ frontend/src/pages/users/UsersPage.js
@@ -86,7 +86,6 @@ const UsersPage = () => {
                 <td>{user.role}</td>
                 <td>{user.active ? "Online" : "Offline"}</td>
                 <td>
-                  {/* Кнопка Edit - доступна для адмінів (включаючи себе) та менеджерів (крім адмінів) */}
                   {(currentUser?.role === "Admin" ||
                     (currentUser?.role === "Company manager" &&
                       user.role !== "Admin")) && (
@@ -98,7 +97,6 @@ const UsersPage = () => {
                     </button>
                   )}
 
-                  {/* Кнопка Delete - доступна тільки для адмінів (крім себе) та менеджерів (крім адмінів) */}
                   {(currentUser?.role === "Company manager" &&
                     user.role !== "Admin") ||
                   (currentUser?.role === "Admin" &&
